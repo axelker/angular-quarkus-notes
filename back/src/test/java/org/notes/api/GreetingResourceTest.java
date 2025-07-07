d6b7e00902a767f125cc584256ddc30443aa8e84
axel <axelker@outlook.fr>
2025-07-07 18:20:33 +0200

    feat: remove it test and move unit controller test

--- back/src/test/java/org/notes/api/GreetingResourceTest.java
+++ back/src/test/java/org/notes/api/GreetingResourceTest.java
@@ -1,10 +1,11 @@
-package org.notes;
+package org.notes.api;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
 
 @QuarkusTest
 class GreetingResourceTest {
